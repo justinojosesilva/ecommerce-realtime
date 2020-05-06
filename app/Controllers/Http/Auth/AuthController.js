25d3fa9618a50a3a12f7970692798f8703d5c279
Justino Jose <justinojosesilva@gmail.com>
2020-05-05 23:20:32 -0300

    AuthController created

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -24,15 +24,27 @@ class AuthController {
     }
 
     async login({ request, response, auth }) {
-
+        const { email, password } = request.all()
+        let data = await auth.withRefreshToken().attempt(email, password)
+        return response.send({ data })
     }
 
     async refresh({ request, response, auth }) {
-
+        let refreshToken = request.input('refresh_token')
+        if (!refreshToken) {
+            refreshToken = request.header('refresh_token')
+        }
+        const user = await auth.newRefreshToken().generateForRefreshToken(refreshToken)
+        return response.send({ data: user })
     }
 
     async logout({ request, response, auth }) {
-
+        let refreshToken = request.input('refresh_token')
+        if (!refreshToken) {
+            refreshToken = request.header('refresh_token')
+        }
+        await auth.authenticator('jwt').revokeTokens([refreshToken], true)
+        return response.status('204').send({})
     }
 
     async forgot({ request, response }) {
