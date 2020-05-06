4d578dd3ad30ec59c589a1f989dba511f13a19be
Justino Jose <justinojosesilva@gmail.com>
2020-05-05 22:46:01 -0300

    Method register created

--- app/Controllers/Http/Auth/AuthAuthController.js
+++ app/Controllers/Http/Auth/AuthAuthController.js
@@ -1,9 +1,26 @@
 'use strict'
+const DataBase = use('DataBase')
+const User = use('App/Models/User')
+const Role = use('Role')
 
 class AuthController {
 
     async register({ request, response }) {
-        //
+        const trx = await DataBase.beginTransaction()
+
+        try {
+            const { name, surname, email, password } = request.all()
+            const user = await User.create({ name, surname, email, password }, trx )
+            const userRoule = await Role.findBy('slug', 'client')
+            await user.roles().attach([userRoule.id], null, trx)
+            await trx.commit()
+            return response.status(201).send({ data: user })
+        } catch (error) {
+            await trx.rollback()
+            return response.status(400).send({
+                message: 'Erro ao realizar cadastro!'
+            })
+        }
     }
 
     async login({ request, response, auth }) {
