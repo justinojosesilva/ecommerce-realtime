7c06edf3db00f9e01735a5a5bba69be4526eadd8
Justino Jose <justinojosesilva@gmail.com>
2020-05-13 22:52:11 -0300

    Add messages in validators

--- app/Validators/Auth/Login.js
+++ app/Validators/Auth/Login.js
@@ -8,6 +8,13 @@ class Login {
       password:'required'
     }
   }
+
+  get messages() {
+    return {
+      'email.required': 'O e-mail já existe',
+      'password.required': 'A senha não foi informada'
+    }
+  }
 }
 
 module.exports = Login
